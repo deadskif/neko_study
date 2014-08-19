38d6bb10091c69c0019f1110a49c6586adeb0e92
Eferina_Ekaterina <eg.eferina@gmail.com>
2014-08-19 20:36:44 +0400

    начали парсить заголовки

--- main.c
+++ main.c
@@ -97,19 +97,67 @@ int main(int argc, char *argv[])
 		if (cfd == -1)
 			handle_error("accept");
 
-           /* Код обработки входящего соединения(й)... */
+           /* Код обработки входящего соединения(й)...*/
 		r = read(cfd, buf, BUF_SIZE);
 		if (r == -1)
 			handle_error("read");
 		if (r > 0) 
-		{
+		{ 	struct header header1;
+			int i, j = 0, fst_space = 0, snd_space = 0, eol = 0;
+			int method_parsed = 0;
 			
+			for (i = 0; buf[i] != '\n'; i++ )
+			{	//printf("buf i %c, k= %d, i= %d, j=%d\n", buf[i], k, i,j);
+				if ((buf[i] == ' ') && (!method_parsed))
+				{
+					if (strncmp(buf,"GET", i) == 0)
+						header1.method = METHOD_GET;
+					else if (strncmp(buf, "POST", i) == 0)
+						header1.method = METHOD_POST;
+					else
+						header1.method = METHOD_UNKNOWN;
+					fst_space = i;
+					method_parsed++;
+					//printf( "metod %d\n", header1.method);
+				}	
+				if ((buf[i] == ' ') && (j != i))
+				{
+					header1.address = (char *)malloc(i - j);
+					header1.address[i - j] = '\0';
+					for (j = fst_space+1; j<i; j++)
+					{
+						header1.address[j - fst_space - 1] = buf[j];
+						//printf("%c", header1.address[n-j+1]);
+					}
+					printf("\n");
+					snd_space = i;
+				}
+				eol = i;			
+					
 			//w = write(cfd, buf, r);
 			//if (w == -1)
 				//handle_error("write");
 			//if (w < r)
 				//printf("something strange\n");
-		}
+			}
+			header1.version = (char *)malloc(eol - snd_space + 1);
+			header1.version[eol - snd_space +1] = '\0';
+			//printf("[%d] = '\\0'\n", n - m +1);
+			//printf("n=%d, m=%d\n", n, m);
+			for (i = snd_space + 1; i <= eol; i++)
+			{
+				header1.version[i - snd_space - 1] = buf[i];
+				//printf("[%d - %d + 1 = %d] = %c", i, m, i - m - 1, buf[i]);
+			}
+#if 0
+			printf("[0]=%#x,", header1.version[0]);
+			for (i = 0; header1.version[i] != '\0'; i++)
+				printf("[%d]=%#x,", i, header1.version[i]);
+			printf("\n");
+#endif
+			printf("metod %d, address '%s', version '%s' \n", header1.method, header1.address, header1.version);
+		}		
+		
 		if (shutdown(cfd, SHUT_RDWR) == -1)
 			handle_error("shutdown_cfd");
 		if (close(cfd) == -1)
