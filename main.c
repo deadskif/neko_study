8f199cfbb3967cf006f4067bec3c02d182d54c96
Eferina_Ekaterina <eg.eferina@gmail.com>
2014-08-21 17:20:13 +0400

    рефакторинг парсинг заголовка

--- main.c
+++ main.c
@@ -102,53 +102,69 @@ int main(int argc, char *argv[])
 		if (r == -1)
 			handle_error("read");
 		if (r > 0) 
-		{ 	struct header header1;
-			int i, j = 0, fst_space = 0, snd_space = 0, eol = 0;
-			int method_parsed = 0;
+		{ 
+			struct header header1 = { .address = NULL, .version = NULL };
+			int i;
+			int method_start = 0;
+			int method_end = 0;
+			int address_start = 0;
+			int address_end = 0;
+			int version_start = 0;
+			int version_end = 0;
+			for (i = 0; buf[i] == ' '; i++)
+				;
+			method_start = i;
+			for (; buf[i] != ' '; i++)
+				;
+			method_end = i;
+			printf("start%d end%d\n", method_start, method_end);
+			if (strncmp(buf + method_start,"GET", method_end - method_start) == 0)
+				header1.method = METHOD_GET; 
+			else if (strncmp(buf + method_start, "POST", method_end - method_start) == 0)
+				header1.method = METHOD_POST;
+			else
+				header1.method = METHOD_UNKNOWN;
 			
-			for (i = 0; buf[i] != '\n'; i++ )
-			{	//printf("buf i %c, k= %d, i= %d, j=%d\n", buf[i], k, i,j);
-				if ((buf[i] == ' ') && (!method_parsed))
-				{
-					if (strncmp(buf,"GET", i) == 0)
-						header1.method = METHOD_GET;
-					else if (strncmp(buf, "POST", i) == 0)
-						header1.method = METHOD_POST;
-					else
-						header1.method = METHOD_UNKNOWN;
-					fst_space = i;
-					method_parsed++;
-					//printf( "metod %d\n", header1.method);
-				}	
-				if ((buf[i] == ' ') && (j != i))
-				{
-					header1.address = (char *)malloc(i - j);
-					header1.address[i - j] = '\0';
-					for (j = fst_space+1; j<i; j++)
-					{
-						header1.address[j - fst_space - 1] = buf[j];
-						//printf("%c", header1.address[n-j+1]);
-					}
-					printf("\n");
-					snd_space = i;
-				}
-				eol = i;			
-					
-			//w = write(cfd, buf, r);
-			//if (w == -1)
-				//handle_error("write");
-			//if (w < r)
-				//printf("something strange\n");
-			}
-			header1.version = (char *)malloc(eol - snd_space + 1);
-			header1.version[eol - snd_space +1] = '\0';
-			//printf("[%d] = '\\0'\n", n - m +1);
-			//printf("n=%d, m=%d\n", n, m);
-			for (i = snd_space + 1; i <= eol; i++)
+			for (; buf[i] == ' '; i++)
+				;
+			address_start = i;
+			for (; buf[i] != ' '; i++)
+				;
+			address_end = i;
+			header1.address = (char *)malloc(address_end - address_start + 1);
+			header1.address[address_end - address_start] = '\0';
+			strncpy(header1.address, buf + address_start, address_end - address_start);
+			
+			for (; buf[i] == ' '; i++)
+				;
+			version_start = i;
+			for (; buf[i] != '\n'; i++)
+				;
+			version_end = i;
+			header1.version = (char *)malloc(version_end - version_start + 1);
+			header1.version[version_end - version_start] = '\0';
+			strncpy(header1.version, buf + version_start, version_end - version_start);
+#if 0
 			{
-				header1.version[i - snd_space - 1] = buf[i];
-				//printf("[%d - %d + 1 = %d] = %c", i, m, i - m - 1, buf[i]);
+				printf("header1.version = %p, header1.address = %p\n", header1.version, header1.address);
+				printf("address_start = %d, [] = %#x\n", address_start, buf[address_start]);
+				printf("address_end = %d, [] = %#x\n", address_end, buf[address_end]);
+				printf("address_diff = %d\n", address_end - address_start);
+				int __i;
+				for (__i = 0; header1.address[__i] != '\0'; __i++)
+					printf("[%d]=%#x('%c'),", __i, header1.address[__i], header1.address[__i]);
+				printf("\n");
+				
+				printf("version_start = %d, [] = %#x\n", version_start, buf[version_start]);
+				printf("version_end = %d, [] = %#x\n", version_end, buf[version_end]);
+				printf("version_diff = %d\n", version_end - version_start);
+				
+				for (__i = 0; header1.version[__i] != '\0'; __i++)
+					printf("[%d]=%#x('%c'),", __i, header1.version[__i], header1.version[__i]);
+				printf("\n");
 			}
+#endif
+
 #if 0
 			printf("[0]=%#x,", header1.version[0]);
 			for (i = 0; header1.version[i] != '\0'; i++)
@@ -156,6 +172,14 @@ int main(int argc, char *argv[])
 			printf("\n");
 #endif
 			printf("metod %d, address '%s', version '%s' \n", header1.method, header1.address, header1.version);
+			if(header1.address) {
+				free(header1.address);
+				header1.address = NULL;
+			}
+			if(header1.version) {
+				free(header1.version);
+				header1.version = NULL;
+			}
 		}		
 		
 		if (shutdown(cfd, SHUT_RDWR) == -1)
