4fd2ae29006e54829c4bcc787f835a2a2d753e56
Eferina_Ekaterina <eg.eferina@gmail.com>
2014-08-29 15:13:35 +0400

    Добавлена поддержка  командной строки и вывод в syslog

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 CC = gcc
 CFLAGS = -W -Wall -O2
+.PHONY: all test
+all: kotya
 kotya: Makefile main.c
 	$(CC) $(CFLAGS) main.c -o $@
+test:
+	echo -en "GET / HTTP/1.1\r\n\r\n" | nc -t 127.0.0.1 8080
+	echo -en "GET /im.jpg HTTP/1.1\r\n\r\n" | nc -t 127.0.0.1 8080
+
--- main.c
+++ main.c
@@ -4,19 +4,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <syslog.h>
 #define KOTYA_PORT 8080
 #define BUF_SIZE 1024
 #define MY_SOCK_PATH "/somepath"
 #define LISTEN_BACKLOG 50
 #define handle_error(msg) \
 	do { perror(msg); exit(EXIT_FAILURE); } while (0)
+#define RESPONSE_INIT { \
+	.status_str = NULL, \
+	.content_type = NULL, \
+	.server_info = "kotya_server", \
+	.head = NULL, \
+	.file_name = NULL, \
+	.filed = -1, \
+	.body_str = NULL \
+}
 enum http_method 
 {
 	METHOD_GET,
 	METHOD_POST,
 	METHOD_UNKNOWN = -1
 };
-	
+int is_suf(const char *str, const char *suf)
+{
+	int str_len = strlen(str);
+	int str_suf = strlen(suf);
+	return strcmp(str + str_len - str_suf, suf) == 0;
+}	
 struct list 
 {
 	char * name;
@@ -60,6 +78,63 @@ struct header
 	struct list *headers;
 	
 };
+struct http_response
+{
+	/*Header*/
+	char *status_str;
+	char *content_type;
+	char *server_info;
+	struct list *head;
+	/*Body*/
+	char *file_name;
+	int filed;
+	char *body_str;
+};
+void write_http_response (int cfd, struct http_response *response, struct header *header1)
+{
+	char buf[BUF_SIZE];
+	int buf_len;
+	int w, r;
+	
+	buf_len = sprintf(buf, "%s %s\r\nContent_Type: %s\r\nServer: %s\r\n", header1->version, response->status_str, response->content_type, response->server_info);
+	buf_len += sprintf(buf + buf_len, "\r\n");
+	if(response->body_str) {
+		buf_len += sprintf(buf + buf_len, "%s", response->body_str);
+	}
+	if((w = write(cfd, buf, buf_len)) < buf_len)
+		handle_error("write");
+	if(response->filed >= 0) {
+		int ffd = response->filed;				
+		while((r = read(ffd, buf, BUF_SIZE)) > 0)
+		{ 
+			if ((w = write(cfd, buf, r)) == -1)
+				handle_error("write_file");
+		}
+		close(ffd);
+	}
+}
+char *get_mimetype_by_name(const char *name)
+{
+    char *mime_type;
+    char *ptr;
+    char buf[BUF_SIZE];
+    sprintf(buf, "mimetype '%s'",name);
+    FILE *fp = popen(buf,"re");
+    if (fp == NULL)
+        handle_error("popen");
+    fgets(buf, BUF_SIZE, fp);
+    buf[strlen(buf)-1] = '\0';
+    //printf("fgets(): '%s'\n", buf);
+    //ssize_t r = getline(&buf, 
+    if ((ptr = strchr(buf,':')) == NULL)
+        handle_error("mimetype_strchr");
+    if ((mime_type = strdup(ptr + 2)) == NULL)
+        handle_error("strdup_mimetype");
+    pclose(fp);
+    return mime_type;
+   
+}
+
 #define HEADER_INIT { \
 	.method = METHOD_UNKNOWN, \
 	.address = NULL, \
@@ -138,13 +213,40 @@ int parse_header(char *buf, struct header *header)
 }
 int main(int argc, char *argv[])
 {
-    int sfd, cfd;
+    int sfd, cfd, ffd;
     struct sockaddr_in my_addr, peer_addr;
     socklen_t peer_addr_size;
     char buf[BUF_SIZE];
     ssize_t r;
     ssize_t w;
-        
+    int flag_n = 0, opt;
+    int port = KOTYA_PORT;
+    pid_t pid;
+   
+    while ((opt = getopt(argc, argv, "p:n")) != -1)
+    {
+        switch (opt)
+        {
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'n':
+                flag_n = 1;
+                break;
+            default: /* '?' */
+                fprintf(stderr, "Usage: %s [-p port]\n",
+                       argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    if (flag_n == 0)
+    {
+        pid = fork();
+        if (pid == -1)
+            handle_error("fork");
+        else if (pid > 0)
+            exit(0);
+    }
     sfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sfd == -1)
 		handle_error("socket");
@@ -152,7 +254,7 @@ int main(int argc, char *argv[])
     /* Очистка структуры */
     
     my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(KOTYA_PORT);
+    my_addr.sin_port = htons(port);
     my_addr.sin_addr.s_addr = INADDR_ANY;
     if (bind(sfd, (struct sockaddr *) &my_addr,
 			sizeof(struct sockaddr_in)) == -1)
@@ -176,6 +278,7 @@ int main(int argc, char *argv[])
 			handle_error("read");
 		if (r > 0) 
 		{ 
+			struct http_response response = RESPONSE_INIT;
 			struct header header1 = HEADER_INIT;
 			struct list *i;
 			parse_header(buf, &header1);
@@ -206,10 +309,34 @@ int main(int argc, char *argv[])
 				printf("[%d]=%#x,", i, header1.version[i]);
 			printf("\n");
 #endif
-			printf("metod %d, address '%s', version '%s' \n", header1.method, header1.address, header1.version);
+			syslog(LOG_DEBUG,"metod %d, address '%s', version '%s' \n", header1.method, header1.address, header1.version);
 			for (i = header1.headers; i != NULL; i = i->next)
-				printf("name '%s' , value '%s' \n", i->name, i->value);
-			if(header1.address) {
+				syslog(LOG_DEBUG,"name '%s' , value '%s' \n", i->name, i->value);
+			if (header1.method == METHOD_GET)
+			{
+				char* address;	
+				int header_len;
+				response.file_name = strtok(header1.address,"?/");
+				if(response.file_name == NULL)
+					response.file_name = "index.html"; 
+				syslog(LOG_DEBUG,"'%s'\n",response.file_name );
+                                response.status_str = "200 OK";
+                                response.content_type = get_mimetype_by_name(response.file_name);
+                                if ((ffd = open(response.file_name, O_RDONLY)) < 0)
+                                {
+                                    response.status_str = "404 Not Found";
+                                    response.body_str = "<html><head><title>Error 404</title></head><body><p>404 Not Found</p></body></html>";
+                                }
+                                response.filed = ffd;
+			}
+			else 
+			{
+				response.status_str = "400 Bad Request";
+				response.body_str = "<html><head><title>Error 400</title></head><body><p>400 Bad Request</p></body></html>";
+			}
+			write_http_response(cfd, &response, &header1);
+			if(header1.address)
+			{
 				free(header1.address);
 				header1.address = NULL;
 			}
@@ -217,13 +344,14 @@ int main(int argc, char *argv[])
 				free(header1.version);
 				header1.version = NULL;
 			}
-		}		
+		}	
+		          
 		
 		if (shutdown(cfd, SHUT_RDWR) == -1)
 			handle_error("shutdown_cfd");
 		if (close(cfd) == -1)
 			handle_error("close_cfd");
-	}	
+	} /* for(;;) */
 	if (shutdown(sfd, SHUT_RDWR) == -1)
 		handle_error("shutdown_sfd");	
 	if (close(sfd) == -1)
